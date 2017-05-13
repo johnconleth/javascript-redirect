511cd4a36da529158c2106939dc5c426dce847f7
jomcat <jomcat@users.noreply.github.com>
2017-05-13 18:34:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// SETTINGS
+$domain = "yourdomain.com"; // The money site that you want to redirect to
+
+// END OF SETTINGS - do not edit below this line
+$q = $_GET['q'];
+$qs = "";
+foreach ($_GET as $key => $value) {
+    if($key != 'q') $qs .= $key."=".$value;
+}
+echo "<html><head><title>Loading...</title></head><body><script>document.location.href = \"http://{$domain}/{$q}?{$qs}\";</script></body></html>";
+exit;
