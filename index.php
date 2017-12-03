b5fc24163bd5ce78d9386275935a89453fd0cb1a
John Conleth <johnconleth@users.noreply.github.com>
2017-12-03 19:09:48 +0000

    Using http_build_query to build querystring

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $domain = "yourdomain.com"; // The money site that you want to redirect to
 
 // END OF SETTINGS - do not edit below this line
 $q = $_GET['q'];
-$qs = "";
+$qs = [];
 foreach ($_GET as $key => $value) {
-    if($key != 'q') $qs .= $key."=".$value;
+    if($key != 'q') $qs[$key] = $value;
 }
-echo "<html><head><title>Loading...</title></head><body><script>document.location.href = \"http://{$domain}/{$q}?{$qs}\";</script></body></html>";
+echo "<html><head><title>Loading...</title></head><body><script>document.location.href = \"http://{$domain}/{$q}?".http_build_query($qs)."\";</script></body></html>";
 exit;
